beb024d35bbb1f347c8cc68f200c11ea594d66ed
eoghan523 <eoghan523@gmail.com>
2024-10-30 12:51:09 +0000

    added to test file

--- week-7/W7-S1-intro-to-tdd/1_Python-TestDrivenDevelopment/assignment/test/test_eoghan_W7S1_assignment.py
+++ week-7/W7-S1-intro-to-tdd/1_Python-TestDrivenDevelopment/assignment/test/test_eoghan_W7S1_assignment.py
@@ -1,3 +1,15 @@
 import math #Imports the built in python math module to allow for basic clauclator functions.
 import pytest  #Imports the pytest module.
 
+#imports eoghan_W7S1_assigment file and imports the functions as listed.
+from eoghan_W7S1_assignment import (
+    factorial, gcd, power )
+
+def test_factorial():
+    assert factorial(10) == 110 # Tests the factorial of 10. test expects 110.
+
+    assert factorial(0) == 1 # Test the factorial of 0. The test expects 1.
+
+    with pytest.raises(ValueError): #tests the valueError for negative integers.
+        factorial(-1)
+

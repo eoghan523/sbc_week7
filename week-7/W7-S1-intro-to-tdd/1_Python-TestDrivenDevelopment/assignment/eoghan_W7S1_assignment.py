de337a6bff73631571f75ffe56806a57dcc934de
eoghan523 <eoghan523@gmail.com>
2024-10-30 12:24:14 +0000

    porgress save

--- week-7/W7-S1-intro-to-tdd/1_Python-TestDrivenDevelopment/assignment/eoghan_W7S1_assignment.py
+++ week-7/W7-S1-intro-to-tdd/1_Python-TestDrivenDevelopment/assignment/eoghan_W7S1_assignment.py
@@ -7,3 +7,11 @@ def factorial(n):
     if n < 0:       #Checks if the input is a negaative integer
         raise ValueError("can't be a negative integer.")  #Raises a value error and message
     result = 1     #Initiates the result to 1.
+    
+      # Iterate from 2 to n (inclusive) to calculate the factorial
+    for i in range(2, n + 1):
+     # Multiply the current result by the current integer i
+        result *= i
+        
+     # Return the factorial 
+    return result

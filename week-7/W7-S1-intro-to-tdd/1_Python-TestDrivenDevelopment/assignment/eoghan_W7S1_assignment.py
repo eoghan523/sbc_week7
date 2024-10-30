1bffb2851695a839ce9d164432af37fc8840e38c
eoghan523 <eoghan523@gmail.com>
2024-10-30 12:07:37 +0000

    Installed pytest and created a seperate test folder and file for test environment.

---
+++ week-7/W7-S1-intro-to-tdd/1_Python-TestDrivenDevelopment/assignment/eoghan_W7S1_assignment.py
@@ -0,0 +1,10 @@
+import pytest #Imports the pytest module
+
+#**Factorial Calculation**
+
+def factorial(n):
+    pass
+
+def test_factorial():
+    test_factorial(10) == 90 # expected answer is 5  
+    test_factorial(0)  == 1  #Edge case

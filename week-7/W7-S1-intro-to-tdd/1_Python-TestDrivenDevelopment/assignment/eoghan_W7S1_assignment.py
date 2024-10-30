1f4b872857bc6f9d639efe3e484c59da28a0e511
eoghan523 <eoghan523@gmail.com>
2024-10-30 12:21:12 +0000

    Save progress

--- week-7/W7-S1-intro-to-tdd/1_Python-TestDrivenDevelopment/assignment/eoghan_W7S1_assignment.py
+++ week-7/W7-S1-intro-to-tdd/1_Python-TestDrivenDevelopment/assignment/eoghan_W7S1_assignment.py
@@ -1,10 +1,9 @@
-import pytest #Imports the pytest module
+import math #Imports the inbuilt python math module
+import pytest #Imports pytest module for use.
 
 #**Factorial Calculation**
 
 def factorial(n):
-    pass
-
-def test_factorial():
-    test_factorial(10) == 90 # expected answer is 5  
-    test_factorial(0)  == 1  #Edge case
+    if n < 0:       #Checks if the input is a negaative integer
+        raise ValueError("can't be a negative integer.")  #Raises a value error and message
+    result = 1     #Initiates the result to 1.
---
+++ week-7/W7-S1-intro-to-tdd/1_Python-TestDrivenDevelopment/assignment/test/test_eoghan_W7S1_assignment.py
@@ -0,0 +1,3 @@
+import math #Imports the built in python math module to allow for basic clauclator functions.
+import pytest  #Imports the pytest module.
+
